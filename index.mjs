5be296ba258e36511c454a12a7680bd462be4b29
jomoartz <johnjohananmogbolu@gmail.com>
2024-12-29 23:15:20 +0100

    updated code to output request method

--- index.mjs
+++ index.mjs
@@ -5,16 +5,21 @@ import url from "node:url";
 const myServer = http.createServer((req, res) => {
   //get the requests url
   const activeurl = req.url;
+  //get the method the user is using
+  const method = req.method.toLowerCase();
 
   //parse the url(to break it into its parts)
   const parsedUrl = url.parse(activeurl, true);
+  console.log(parsedUrl);
   //get the url pathname
   const urlPathname = parsedUrl.pathname;
   //trim url to remove excess slashes
   const trimmedUrl = urlPathname.replace(/^\/+\/+$/g, "");
   console.log(trimmedUrl);
   // return a response telling the user they've been rediected to another url.
-  res.end(`Hello, you have been redirected to: ${trimmedUrl} `);
+  res.end(
+    `Hello, you have been redirected to: ${trimmedUrl} method:${method} `
+  );
 });
 
 myServer.listen(4190, () => {
