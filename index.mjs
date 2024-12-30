d549bb021da96ce179c17521ea68573e90764d87
jomoartz <johnjohananmogbolu@gmail.com>
2024-12-30 13:33:47 +0100

    updated code to logg payload

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import * as http from "http";
 import url from "node:url";
+import { StringDecoder } from "node:string_decoder";
 
 //this server should responed to all requests with a string
 const myServer = http.createServer((req, res) => {
@@ -9,21 +10,38 @@ const myServer = http.createServer((req, res) => {
   const method = req.method.toLowerCase();
   //get the headers
   const reqHeader = req.headers;
+  //initialize a decoder
+  const decoder = new StringDecoder("utf8");
+  //get the payload
+  let payload = ""; //variabe to store the payload
+  req.on("data", (data) => {
+    //decode the data to utf-8 charset
+    payload += decoder.write(data);
+  });
+  //listen for when payload is done sending
+  req.on("end", () => {
+    payload += decoder.end();
 
-  //parse the url(to break it into its parts)
-  const parsedUrl = url.parse(activeurl, true);
-  //get the url pathname
-  const urlPathname = parsedUrl.pathname;
-  //get the url query string
-  const queryString = parsedUrl.query;
+    //parse the url(to break it into its parts)
+    const parsedUrl = url.parse(activeurl, true);
+    //get the url pathname
+    const urlPathname = parsedUrl.pathname;
+    //get the url query string
+    const queryString = parsedUrl.query;
 
-  //trim url to remove excess slashes
-  const trimmedUrl = urlPathname.replace(/^\/+\/+$/g, "");
-  console.log(trimmedUrl);
-  // return a response telling the user they've been rediected to another url.
-  res.end(`Hello, you have been redirected to: ${trimmedUrl} method:${method}`);
-  console.log("req comes with this headers: ", reqHeader);
-  console.log("req comes with this query: ", queryString);
+    //trim url to remove excess slashes
+    const trimmedUrl = urlPathname.replace(/^\/+\/+$/g, "");
+
+    console.log(trimmedUrl);
+    console.log("req comes with this headers: ", reqHeader);
+    console.log("req comes with this query: ", queryString);
+    console.log("req comes with this payload:", payload);
+
+    // return a response telling the user they've been rediected to another url.
+    res.end(
+      `Hello, you have been redirected to: ${trimmedUrl} method:${method}`
+    );
+  });
 });
 
 myServer.listen(4190, () => {
